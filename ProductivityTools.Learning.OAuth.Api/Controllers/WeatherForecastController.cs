083b14f191fdcd63f44e4bd23c2dfa635a8d52f0
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-04-29 21:49:39 +0200

    initial commit project

---
+++ ProductivityTools.Learning.OAuth.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProductivityTools.Learning.OAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public DateTime Get()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ ProductivityTools.Learning.OAuth.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductivityTools.Learning.OAuth.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
