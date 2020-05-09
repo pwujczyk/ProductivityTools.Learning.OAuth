334ae4e4320715130de92ba991c95974420bb983
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-09 21:35:21 +0200

    identity server

--- ProductivityTools.IdentityServer/Config.cs
+++ ProductivityTools.IdentityServer/Config.cs
@@ -11,17 +11,36 @@ namespace ProductivityTools.IdentityServer
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
+            new List<ApiResource>
+            {
+                new ApiResource("api1", "My api")
+            };
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
+            new List<Client>
+            {
+                new Client
+                {
+                      ClientId = "client",
+
+                      // no interactive user, use the clientid/secret for authentication
+                      AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                      // secret for authentication
+                      ClientSecrets =
+                      {
+                        new Secret("secret".Sha256())
+                      },
+
+                      // scopes that client has access to
+                      AllowedScopes = { "api1" }
+                }
+            };
+
     }
 }
